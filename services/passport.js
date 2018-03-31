a67f1dc9242b9014bddddea726e33dbe68ee4fee
Manuel <manuel1412@gmail.com>
2018-03-31 15:31:14 +0100

    37. First mongoose query

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,13 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //found one
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
