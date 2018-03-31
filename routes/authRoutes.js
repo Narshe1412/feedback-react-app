0f10cb21f49625f0e3582bf93d7d12a7e565155b
Manuel <manuel1412@gmail.com>
2018-03-31 16:34:14 +0100

    43. Logout api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,9 @@ module.exports = app => {
   app.get('/api/currentuser', (req, res) => {
     res.send(req.user);
   })
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
 };
