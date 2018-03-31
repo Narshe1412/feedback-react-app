ce538011b261b60245eb28bc6df628e228d41bce
Manuel <manuel1412@gmail.com>
2018-03-31 08:14:30 +0100

    22. Created google strategy options

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: 'auth/google/callback'
+  }),
+  accessToken => {
+    console.log(accessToken);
+  }
+);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
@@ -12,5 +22,3 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
